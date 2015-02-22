a530344e4a3351c68da2dc9b1bf035e8d500d826
Ursula Sarracini <ursulasarracini@gmail.com>
2015-02-21 22:37:42 -0500

    added comments

--- fcfs.c
+++ fcfs.c
@@ -13,7 +13,7 @@
 #include <assert.h>
 #include "helper.h"
 
-// some useful global variables
+// Some useful global variables
 Process processes[MAX_PROCESSES+1];
 int numberOfProcesses = 0;
 int nextProcess = 0;
@@ -23,18 +23,20 @@ int cpuTimeUtilized = 0;
 int theClock = 0;
 int sumTurnarounds = 0;
 
+// Ready process queue and waiting process queue
 Process_queue readyQueue;
 Process_queue waitingQueue;
 
+// CPU's
 Process *CPUS[NUMBER_OF_PROCESSORS];
 
+// Temporary "Pre-Ready" queue
 Process *tmpQueue[MAX_PROCESSES+1];
 int tmpQueueSize;
 
-/* to do:
-- clean up code and add comments
-*/
-
+/**
+ * Creates a single process node with pointer to data and next
+ */
 Process_node *createProcessNode(Process *p){
 	Process_node *node = (Process_node*)malloc(sizeof(Process_node));
 	if (node == NULL){
@@ -44,7 +46,9 @@ Process_node *createProcessNode(Process *p){
 	node->next = NULL;
 	return node;
 }
-
+/**
+ * Resets all global variables to 0 
+ */
 void resetVariables(void){
 	numberOfProcesses = 0;
 	nextProcess = 0;
@@ -55,13 +59,17 @@ void resetVariables(void){
 	sumTurnarounds = 0;
 	tmpQueueSize = 0;
 }
-
+/**
+ * Initializes a process queue. Makes an empty queue
+ */
 void initializeProcessQueue(Process_queue *q){
 	q = (Process_queue*)malloc(sizeof(Process_queue));
 	q->front = q->back = NULL;
 	q->size = 0;
 }
-
+/**
+ * Equeues a process
+ */
 void enqueueProcess(Process_queue *q, Process *p){
 	Process_node *node = createProcessNode(p);
 	if (q->front == NULL){
@@ -75,7 +83,9 @@ void enqueueProcess(Process_queue *q, Process *p){
 	}
 	q->size++;
 }
-
+/**
+ * Dequeues a process
+ */
 void dequeueProcess(Process_queue *q) {
     Process_node *deleted = q->front;
     assert(q->size > 0);
@@ -89,26 +99,37 @@ void dequeueProcess(Process_queue *q) {
     free(deleted);
     q->size--;  
 }
-
+/**
+ * Calulates average wait time 
+ */
 float averageWaitTime(int theWait){
 	float result = theWait / (double) numberOfProcesses;
 	return result;
 }
-
+/**
+ * Calculates average turnaround time
+ */
 float averageTurnaroundTime(int theTurnaround){
 	float result = theTurnaround / (double) numberOfProcesses;
 	return result;
 }
- 
+ /**
+  * Calcualtes average CPU utilization
+  */
 float averageUtilizationTime(int theUtilization){
  	float result = (theUtilization / theClock) * 100.0;
  	return result;
  }
-
+/**
+ * Return the total number of incoming processes. These processes have yet
+ * to arrive in the system
+ */
 int totalIncomingProcesses(void){
 	return numberOfProcesses - nextProcess;
 }
-
+/**
+ * Compare arrival time of two processes
+ */
 int compareArrivalTime(const void *a, const void *b){
 	Process *first = (Process *) a;
 	Process *second = (Process *) b;
@@ -120,7 +141,9 @@ int compareArrivalTime(const void *a, const void *b){
 	}
 	return 0;
 }
-
+/**
+ * Compare process ID of two processes
+ */
 int compareProcessIds(const void *a, const void *b){
 	Process *first = (Process *) a;
 	Process *second = (Process *) b;
@@ -132,7 +155,10 @@ int compareProcessIds(const void *a, const void *b){
 		return -1;
 	}
 }
-
+/**
+ * Iterates over all CPU's and to find and return the total number of 
+ * currently running processes
+ */
 int runningProcesses(void){
 	int runningProcesses = 0;
 	int i;
@@ -143,7 +169,11 @@ int runningProcesses(void){
 	}
 	return runningProcesses;
 }
-
+/**
+ * Grabs the next scheduled process in the queue (first process currently at
+ * the front of the ready queue). Increments the waiting time in order to update
+ * the ready state. Returns the next process to be run
+ */
 Process *nextScheduledProcess(void){
 	if (readyQueue.size == 0){
 		return NULL;
@@ -155,13 +185,21 @@ Process *nextScheduledProcess(void){
 		return grabNext;
 	}
 }
-
+/**
+ * Add any new incoming processes to a temporary queue to be sorted and later added
+ * to the ready queue. These incoming processes are put in a "pre-ready queue"
+ */
 void addNewIncomingProcess(void){
 	while(nextProcess < numberOfProcesses && processes[nextProcess].arrivalTime <= theClock){
 		tmpQueue[tmpQueueSize++] = &processes[nextProcess++];
 	}
 }
-
+/**
+ * Get the first process in the waiting queue, check if their I/O burst is complete.
+ * If the current I/O burst is complete, move on to next I/O burst and add the process
+ * to the "pre-ready queue". Dequeue the waiting queue and update waiting state by 
+ * incrementing the current burst's step
+ */
 void waitingToReady(void){
  	int i;
  	for(i = 0; i < waitingQueue.size; i++){
@@ -177,7 +215,12 @@ void waitingToReady(void){
  		grabNext->bursts[grabNext->currentBurst].step++;
  	}
  }
-
+/**
+ * Sort elements in "pre-ready queue" in order to add them to the ready queue
+ * in the proper order. Enqueue all processes in "pre-ready queue" to ready queue.
+ * Reset "pre-ready queue" size to 0. Find a CPU that doesn't have a process currently 
+ * running on it and schedule the next process on that CPU
+ */
 void readyToRunning(void){
  	int i;
  	qsort(tmpQueue, tmpQueueSize, sizeof(Process*), compareProcessIds);
@@ -191,7 +234,12 @@ void readyToRunning(void){
  		}
  	}
  }
-
+/**
+ * If a process has finished their CPU burst, move them to the waiting queue and terminate
+ * those who have finished their CPU burst. Start the process' next I/O burst. If CPU burst is
+ * not finished, move the process to the waiting queue and free the current CPU. If the CPU burst
+ * is finished, terminate the process by setting the end time to the current simulation time
+ */
 void runningToWaiting(void){
  	int i;
  	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
@@ -210,7 +258,11 @@ void runningToWaiting(void){
  		}	
  	}
  }
-
+/**
+ * Display results for average waiting time, average turnaround time, the time
+ * the CPU finished all processes, average CPU utilization, number of context
+ * switches, and the process ID of the last process to finish.
+ */
 void displayResults(float awt, float atat, int sim, float aut, int cs, int pids){
 	printf( "Average waiting time:   %.2f units\n"
 		"Average turnaround time:   %.2f units\n"
@@ -226,7 +278,7 @@ int main(){
 	float ut, wt, tat;
 	int lastPID;
 	
-	// clear CPU'S, and initialize queues
+	// clear CPU'S, initialize queues, and reset global variables
 	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
 		CPUS[i] = NULL;
 	}
@@ -253,6 +305,7 @@ int main(){
 		waitingToReady();
 		readyToRunning();
 
+		// break when there are no more running or incoming processes, and the waiting queue is empty
 		if (runningProcesses() == 0 && totalIncomingProcesses() == 0 && waitingQueue.size == 0){
 			break;
 		}
